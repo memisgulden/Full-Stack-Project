33ade2a4f552d46d7f6e27af5df145ec25b4ae70
Kaitlyn McLaughlin <kaitlyn@Kaitlyns-MacBook-Air.local>
2018-11-10 11:36:27 -0500

    adding project

---
+++ models/example.js
@@ -0,0 +1,8 @@
+module.exports = function(sequelize, DataTypes) {
+    var Example = sequelize.define("Example", {
+      text: DataTypes.STRING,
+      description: DataTypes.TEXT
+    });
+    return Example;
+  };
+  
