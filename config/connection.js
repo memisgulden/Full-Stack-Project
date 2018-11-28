b975d7862f945e452d6337bcc3ef593a2ecb4129
bilu k <contactnebey@yahoo.com>
2018-11-27 21:15:56 -0500

    up

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,28 @@
 // Dependencies
 var Sequelize = require("sequelize");
 
-// Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("lunch_db", "root", "password", {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+// Creates mysql connection
+if(process.env.JAWSDB_URL){
+   connection = mysql.createConnection(process.env.JAWSDB_URL);
+ // the application is executed on Heroku ... use the mysql database
+//  sequelize = new Sequelize(process.env.HEROKU_JAWSDB_URL, {
+//    dialect:  'mysql',
+//    protocol: 'mysql',
+//    port:     3306,
+//    host:     "h2cwrn74535xdazj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//    logging:  true
+//  })
+} else {
 
+ sequelize = new Sequelize("lunch_db", "root", "mySQL", {
+   host: "localhost",
+   dialect: "mysql",
+   pool: {
+     max: 5,
+     min: 0,
+     idle: 10000
+   }
+ });
+};
 // Exports the connection for other files to use
 module.exports = sequelize;
