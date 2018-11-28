23818b29f6df383f2bbe51bebb2752b3b357aebb
bilu k <contactnebey@yahoo.com>
2018-11-28 10:15:41 -0500

    fixing

--- config/connection.js
+++ config/connection.js
@@ -20,33 +20,5 @@ if (process.env.JAWSDB_URL) {
 
 
 }
-
-<<<<<<< HEAD
-// Creates mysql connection
-if(process.env.JAWSDB_URL){
-   connection = mysql.createConnection(process.env.JAWSDB_URL);
- // the application is executed on Heroku ... use the mysql database
- sequelize = new Sequelize(process.env.HEROKU_JAWSDB_URL, {
-   dialect:  'mysql',
-   protocol: 'mysql',
-   port:     3306,
-   host:     "h2cwrn74535xdazj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-   logging:  true
- })
-} else {
-=======
-// Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
->>>>>>> dadd0919648f715180745617a2f67df9d0081878
-
- sequelize = new Sequelize("lunch_db", "root", "mySQL", {
-   host: "localhost",
-   dialect: "mysql",
-   pool: {
-     max: 5,
-     min: 0,
-     idle: 10000
-   }
- });
-};
 // Exports the connection for other files to use
 module.exports = sequelize;
