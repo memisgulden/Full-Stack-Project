56508b8ca88d8920e9e8cf727d337a3ab1c595b5
bilu k <contactnebey@yahoo.com>
2018-11-27 21:17:52 -0500

    update

--- config/connection.js
+++ config/connection.js
@@ -5,13 +5,13 @@ var Sequelize = require("sequelize");
 if(process.env.JAWSDB_URL){
    connection = mysql.createConnection(process.env.JAWSDB_URL);
  // the application is executed on Heroku ... use the mysql database
-//  sequelize = new Sequelize(process.env.HEROKU_JAWSDB_URL, {
-//    dialect:  'mysql',
-//    protocol: 'mysql',
-//    port:     3306,
-//    host:     "h2cwrn74535xdazj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-//    logging:  true
-//  })
+ sequelize = new Sequelize(process.env.HEROKU_JAWSDB_URL, {
+   dialect:  'mysql',
+   protocol: 'mysql',
+   port:     3306,
+   host:     "h2cwrn74535xdazj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+   logging:  true
+ })
 } else {
 
  sequelize = new Sequelize("lunch_db", "root", "mySQL", {
