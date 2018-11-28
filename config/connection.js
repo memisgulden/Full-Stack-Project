c05ceb737296bf122c8ab55c3a6a44e854ad56ae
Kaitlyn McLaughlin <kaitlyn@Kaitlyns-MacBook-Air.local>
2018-11-27 19:26:07 -0500

    adding stuff

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,23 @@
 // Dependencies
 var Sequelize = require("sequelize");
 
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306, 
+    user: "root",
+    password: "password",
+    database: "lunch_db"
+  })
+}
+
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
 var sequelize = new Sequelize("lunch_db", "root", "password", {
-  host: "localhost",
-  port: 3306,
+  host: "l6slz5o3eduzatkw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: ""
   dialect: "mysql",
   pool: {
     max: 5,
